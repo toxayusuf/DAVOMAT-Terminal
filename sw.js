f8ca5d6328eeabdff5535cceb125a25699af438a
toxayusuf <t.mamutxanov@gmail.com>
2026-09-15 17:35:09 +0500

    DAVOMAT v0.8.1: refresh service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='davomat-terminal-v0.8.0';
+const CACHE='davomat-terminal-v0.8.1';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
