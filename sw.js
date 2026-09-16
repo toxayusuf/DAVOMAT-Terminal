d2b3374442842053373f5e04ce65bbeffa30fb9a
toxayusuf <t.mamutxanov@gmail.com>
2026-09-16 12:57:59 +0500

    DAVOMAT v1.2.1: refresh cache for portable device config

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='davomat-terminal-v1.2.0';
+const CACHE='davomat-terminal-v1.2.1';
 const SHELL=['./','./index.html','./styles.css','./app.js','./top-level-fix.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
